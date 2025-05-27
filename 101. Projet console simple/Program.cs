90e07b73eb50ab21f62c17be0faf4300e8110c7c
la404family <150837754+la404family@users.noreply.github.com>
2025-05-27 23:13:07 +0200

    mise en place d'un projet

---
+++ 101. Projet console simple/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+// Recuperer le nom de l'utilisateur 
+Console.Write("Quel est votre nom ? ");
+string username = Console.Readline();
+//Afficher un message de bienvenue
+Console.WriteLine("Hello, World! mais surtout bonjour : " + username);
+
+
