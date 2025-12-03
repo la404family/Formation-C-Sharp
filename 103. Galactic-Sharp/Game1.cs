1886b4247c7f0b005d16bbf6b57cac11aa39b492
la404family <150837754+la404family@users.noreply.github.com>
2025-12-03 02:22:33 +0100

    change

---
+++ 103. Galactic-Sharp/Game1.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace _103._Galactic_Sharp;
+
+public class Game1 : Game
+{
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+
+    public Game1()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        // Configuration de la fenêtre
+        Window.Title = "Galactic-Sharp";
+        Window.AllowUserResizing = false;
+
+        // Résolution 720p
+        _graphics.PreferredBackBufferWidth = 1280;
+        _graphics.PreferredBackBufferHeight = 720;
+        _graphics.ApplyChanges();
+
+        // Centrage de la fenêtre
+        // Note : GraphicsAdapter.DefaultAdapter.CurrentDisplayMode donne la résolution de l'écran principal
+        int screenWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+        int screenHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+
+        Window.Position = new Point(
+            (screenWidth - _graphics.PreferredBackBufferWidth) / 2,
+            (screenHeight - _graphics.PreferredBackBufferHeight) / 2
+        );
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        // TODO: use this.Content to load your game content here
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        // TODO: Add your update logic here
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        // TODO: Add your drawing code here
+
+        base.Draw(gameTime);
+    }
+}
---
+++ 103. Galactic-Sharp/Program.cs
@@ -0,0 +1,2 @@
+﻿using var game = new _103._Galactic_Sharp.Game1();
+game.Run();
